d86e98d2fafd452ebdd8bc4e251ac980302d95bc
kimminseok <alstjr706@naver.com>
2026-06-21 13:48:07 +0900

    feat: 단건 상품(Product) 엔티티 추가

---
+++ src/main/java/com/example/payment/product/entity/Product.java
@@ -0,0 +1,35 @@
+package com.example.payment.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Builder
+    private Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
